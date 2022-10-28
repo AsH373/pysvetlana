8cd2f2deb15856a519fe7ae050138b7c9a4fdd73
AsH373 <andrey.lazarev19@icloud.com>
2022-10-28 20:58:59 +0300

    Добавлен файл

---
+++ main.py
@@ -0,0 +1,10 @@
+
+import pyTelegramBotAPI
+# import telebot
+
+bot =  pyTelegramBotAPI.TeleBot('5688000369:AAFMhLZ4Nbvf3BTM0GcQSf5x8mOahKhZwL8')
+@bot.message_handler(comamnds = ['start'])
+@bot.message_handler(content_types = ['text'])
+def repeat(message):
+    bot.send_message(message.chat.id, message.text)
+bot.polling(non_stop = True)
